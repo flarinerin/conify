714f5757861df67a6c6d0996d79b7be9a29513d2
Sam Clopton <samsinite@gmail.com>
2016-06-02 21:57:05 -0700

    First pass at adding the router main view, and a few initial views. Implements main view with routes so that work on implementing story #26 can begin

---
+++ client/app/bundles/ConifyClient/components/Event.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Event extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="events">
+        This is where a single event will be.
+      </div>
+    );
+  }
+}
+
+export default Event;
