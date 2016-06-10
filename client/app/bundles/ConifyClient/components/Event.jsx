b27bcced0c667bae90b3b7ceb8f1ce9700fd7f1d
kevin <harokevin0@gmail.com>
2016-06-10 16:32:18 +0000

    Update Event Component

--- client/app/bundles/ConifyClient/components/Event.jsx
+++ client/app/bundles/ConifyClient/components/Event.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 
 class Event extends React.Component {
+  
   constructor(props) {
     super(props);
+    this.state = { isBookmarked: false };
+    this.onBookmark = this.onBookmark.bind(this);
   }
-
+  
+  onBookmark(){
+    this.setState({ isBookmarked: !this.state.isBookmarked });
+  }
+  
+  //[Title]     [Bookmark toggle]
+  //[VenueName]       [TrackName]
   render() {
     return (
-      
-      //Title         [Bookmark toggle]
-      //VenueName     [TrackName]
       <div class="events">
-        
+        {/*[Title]  [Bookmark toggle]*/}
         <div className="row">
-          {/*Title         [Bookmark toggle]*/}
-          <div className="col-xs-6">Why do my cats keep running away?</div>
-          <div className="col-xs-6">BMIcon</div>
+          <div className="col-xs-6">{this.props.title}</div>
+          <div className="col-xs-6" onClick={this.onBookmark} id="bookmark">{
+            this.state.isBookmarked ? 
+              <i  className="fa fa-bookmark">marked</i> :
+              <i  className="fa fa-bookmark-o">unmarked</i>
+          }
+          </div>
         </div>
-        
-        <div className="row">
-          {/*VenueName     [TrackName]*/}
-          <div className="col-xs-6">Awesomesauce Concert Hall and Theater</div>
-          <div className="col-xs-6">Pets</div>
+        {/*[VenueName]  [TrackName]*/}
+        <div className="row"> 
+          <div className="col-xs-6">{this.props.venueName}</div>
+          <div className="col-xs-6">{this.props.track}</div>
         </div>
       </div>
     );
   }
 }
-
 
 export default Event;
