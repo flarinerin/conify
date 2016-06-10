48ed38c6087a46f1ecb9eb8752abc374ee1b0c93
kevin <harokevin0@gmail.com>
2016-06-10 15:46:45 +0000

    Revert "Merge branch 'EventShow-skeleton'"
    This reverts commit bd3a6a4da156e04af531c0ec85b907f87db6e830, reversing
    changes made to ef22954afd581a28e743d48bb61ac59854ff37d5.

--- client/app/bundles/ConifyClient/components/Events.jsx
+++ client/app/bundles/ConifyClient/components/Events.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import Event from './Event';
 
 class Events extends React.Component {
   constructor(props) {
     super(props);
-    this.handleBookmark = function(eventName){ alert('EVENT bookmarked(Event speaking!)'.concat(eventName)); };
   }
-  
+
   render() {
     return (
       <div className="events">
         This is where the events will be.
-        <Event  title="Awesome Event Title" isBookmarked="false"
-                venueName ="Awesome Venue Name" track="Technology"/>
         <button className="btn btn-default" >This is the default button</button>
         <div className="dropdown">
           <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
