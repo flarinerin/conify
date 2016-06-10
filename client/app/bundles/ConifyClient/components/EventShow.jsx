b53ea76fa0747af0c66fe8b05b20965b529d802b
kevin <harokevin0@gmail.com>
2016-06-10 22:10:57 +0000

    Added EventShow Component

---
+++ client/app/bundles/ConifyClient/components/EventShow.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class EventShow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="event-show">
+        This is where the event show will be.
+      </div>
+    );
+  }
+}
+
+export default EventShow;
