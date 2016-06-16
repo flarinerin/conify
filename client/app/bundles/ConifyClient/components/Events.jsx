91ffd09e49e813d7ec5b72a1ccd2bd44ef5680b6
Thomas Suckow <tsuckow@gmail.com>
2016-06-16 02:45:32 +0000

    Layout html for events list

--- client/app/bundles/ConifyClient/components/Events.jsx
+++ client/app/bundles/ConifyClient/components/Events.jsx
@@ -9,18 +9,13 @@ class Events extends React.Component {
   render() {
     return (
       <div className="events">
-        This is where the events will be.
-        <Event / >
-        <button className="btn btn-default" >This is the default button</button>
-        <div className="dropdown">
-          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </button>
-          <div className="dropdown-menu" aria-labelledby="dropdownMenu1">
-            <a className="dropdown-item" href="#">Action</a>
-            <a className="dropdown-item" href="#">Another action</a>
-            <a className="dropdown-item" href="#">Something else here</a>
-          </div>
+        <header>
+          <span><i className="fa fa-bars"></i></span>
+          <h2>Event Schedule</h2>
+          <span><i className="fa fa-bookmark"></i></span>
+        </header>
+        <div className="event-list">
+          <Event />
         </div>
       </div>
     );
