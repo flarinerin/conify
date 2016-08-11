8fb3f4e951da272f3bbb6bf219aa2f67e4a845a2
Thomas Suckow <tsuckow@gmail.com>
2016-08-11 03:29:10 +0000

    Style the date picker

--- client/app/bundles/ConifyClient/components/DatePicker.jsx
+++ client/app/bundles/ConifyClient/components/DatePicker.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import moment from 'moment';
 import map from 'lodash/fp/map';
 import range from 'lodash/fp/range';
 
 function formatDate(date,days) {
   let m = date.clone().add(days,'days');
-  return <div key={m} className="date-picker-date"><span>{m.format('ddd')}</span><span>{m.format('D')}</span></div>;
+  let size = '100%';
+  switch(days) {
+  case -2:
+  case 2:
+    size = '50%';
+    break;
+  case 1:
+  case -1:
+    size = '80%';
+    break;
+  }
+  const style = { fontSize: size };
+  return <div key={m} className="date-picker-date" style={style}><span>{m.format('ddd')}</span><span>{m.format('D')}</span></div>;
 }
 
 export default class DatePicker extends React.Component {
@@ -31,7 +46,9 @@ export default class DatePicker extends React.Component {
     return (
       <div className="date-picker">
         <button onClick={this.prev}><i className="fa fa-caret-left"></i></button>
-        {pickers}
+        <ReactCSSTransitionGroup className="date-picker-dates" transitionName="date-picker" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
+          {pickers}
+        </ReactCSSTransitionGroup>
         <button onClick={this.next}><i className="fa fa-caret-right"></i></button>
       </div>
     );
